1724b1ae5f28caf1a2e83f0514a3ec4c5984085a
andnane adjana <andnaneadjana@yahoo.fr>
2020-09-24 23:41:07 -0400

    remove console.log in js front end

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,3 @@
-console.log("Hello, javascript from front-end");
-
-
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const message1 = document.querySelector("#msg1");
